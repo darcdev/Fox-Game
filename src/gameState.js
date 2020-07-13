04aeca55640ff812d7fdad9791b893222a6e73a2
darcdev <diegoandresrojas2000@gmail.com>
2020-07-13 08:17:06 -0500

    transitioning between states

--- src/gameState.js
+++ src/gameState.js
@@ -1,11 +1,88 @@
+import { modFox, modScene } from "./ui";
+import {
+  RAIN_CHANCE,
+  SCENES,
+  DAY_LENGTH,
+  NIGHT_LENGTH,
+  getNextDieTime,
+  getNextHungerTime,
+} from "./constants";
+
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
+  sleepTime: -1,
+  hungryTime: -1,
+  dieTime: -1,
   tick() {
     this.clock++;
-    console.log("clock", this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    } else if (this.clock === this.sleepTime) {
+      this.sleepTime();
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    } else if (this.clock === this.dieTime) {
+      this.die();
+    }
     return this.clock;
   },
+  startGame() {
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+    modFox("egg");
+    modScene("day");
+  },
+  sleep() {
+    this.state = "SLEEP";
+    modFox("sleep");
+    modScene("night");
+    this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+  wake() {
+    console.log("Awoken");
+    this.current = "IDLING";
+    this.wakeTime = -1;
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
+    this.sleepTime = this.clock + DAY_LENGTH;
+  },
+  handleUserAction(icon) {
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      return;
+    }
+
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+  changeWeather() {
+    console.log("change weather");
+  },
+  cleanUpPoop() {
+    console.log("cleanup poop");
+  },
+  feed() {
+    console.log("feed");
+  },
 };
+
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 
 export default gameState;
---
+++ src/ui.js
@@ -0,0 +1,10 @@
+export const modFox = function modfox(state) {
+  document.querySelector(".fox").className = `fox fox-${state}`;
+};
+export const modScene = function modScene(state) {
+  document.querySelector(".game").className = `game ${state}`;
+};
+
+export const togglePoopBag = function togglePoopBag(show) {
+  document.querySelector(".poop-bag").classList.toggle("hidden", !show);
+};
